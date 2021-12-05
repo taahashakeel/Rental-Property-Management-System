51eb5073ccdad72301e5d507edfc60fb46577766
Ethan Sengsavang <eth.sengsavang@gmail.com>
2021-12-05 01:59:37 -0700

    Add GUI class file with minor comments

---
+++ src/gui/GUI.java
@@ -0,0 +1,13 @@
+/**
+ * @author Ethan Sengsavang <a href="mailto:ethan.sengsavang@ucalgary.ca">email</a>
+ *
+ * The base GUI frame to be populated with Java AWT Components that perform
+ * functions specific to the user who is logged in.
+ *
+ * Assets to populate the GUI with will be specified by an UserPanel instance
+ * found within every user class.
+ */
+
+package gui;
+
+import java.awt.Component;
