ec8994e150e61cc26ef7fb5082e6eead22f7d8e1
Ethan Sengsavang <eth.sengsavang@gmail.com>
2021-12-05 02:25:15 -0700

    Define base-bones WindowListener class

---
+++ src/gui/WindowListener.java
@@ -0,0 +1,17 @@
+/**
+ * @author Ethan Sengsavang <a href="mailto:ethan.sengsavang@ucalgary.ca">email</a>
+ *
+ * Listens for events that occur to a specific window.
+ *
+ * More specifically, listen for the event to close the current window.
+ */
+
+package gui;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+
+public class WindowListener extends WindowAdapter{
+}
