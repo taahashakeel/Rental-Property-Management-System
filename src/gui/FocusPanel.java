b935dfb6f3740736b915b30fec87f73992308632
e-seng <eth.sengsavang@gmail.com>
2021-12-06 12:13:06 -0700

    Define addWidget method to simplify widget adding

--- src/gui/FocusPanel.java
+++ src/gui/FocusPanel.java
@@ -6,6 +6,8 @@
  */
 package gui;
 
+import javax.swing.Component;
+
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 
@@ -16,4 +18,24 @@ public abstract class FocusPanel extends JPanel{
   private GridBagConstraints panelConstraints;
 
   public renderDisplay(GUI frame);
+
+  /**
+   * Adds a specified widget into the panel at the specified position
+   * and size.
+   *
+   * @param widget The widget to add within the window
+   * @param x The horizontal index within the grid to place the widget at.
+   * @param y The vertical index within the grid to place the widget at.
+   * @param w The width of the widget.
+   * @param h The height of the widget.
+   */
+  public void addWidget(Component widget, int x, int y, int w, int h){
+    panelConstraints.gridx = x;
+    panelConstraints.gridy = y;
+    panelConstraints.gridwidth = w;
+    panelConstraints.gridheight = h;
+
+    panelLayout.setConstraints(widget, panelConstraints);
+    add(widget);
+  }
 }
