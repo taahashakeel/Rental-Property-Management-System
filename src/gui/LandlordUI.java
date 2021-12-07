32e6d08ac9c62dee25615e71f94721e8d1170cb5
e-seng <eth.sengsavang@gmail.com>
2021-12-07 14:06:30 -0700

    Add empty LandlordUI class extending FocusPanel
    this is another user UI

---
+++ src/gui/LandlordUI.java
@@ -0,0 +1,17 @@
+/**
+ * @author Ethan Sengsavang
+ * <a href="mailto:ethan.sengsavang@ucalgary.ca">email</a>
+ *
+ * The user interface landlords will use to use the Rental Property Management
+ * System.
+ *
+ * This UI should allow Landlords to create properties and view existing
+ * property details.
+ */
+package gui;
+
+public class LandlordUI extends FocusPanel{
+  public LandlordUI(){
+    super();
+  }
+}
