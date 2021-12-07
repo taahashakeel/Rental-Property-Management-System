77498d47fc0b935d7635cae896fa7a9e3e27ca77
e-seng <eth.sengsavang@gmail.com>
2021-12-06 17:29:12 -0700

    Create blank SearchMenu gui element

---
+++ src/gui/elements/SearchMenu.java
@@ -0,0 +1,21 @@
+/**
+ * @author Ethan Sengsavang
+ * <a href="mailto:ethan.sengsavang@ucalgary.ca">email</a>
+ *
+ * The menu for users to search up different types of properties they may be
+ * interested in.
+ *
+ * With this menu, interested renters should be able to filter properties by
+ * - Housing type: Apartment, Attached/Detached House, Townhouse, Condo
+ * - Number of Bedrooms: (at least, at most, exactly a number)
+ * - Number of Bathrooms: (at least, at most exactly)
+ * - Furnishing state: Furnished or Unfurnished.
+ * - City Quadrant: NE, NW, SW, SE
+ */
+package gui.elements;
+
+public class SearchMenu extends JPanel{
+  public SearchMenu(){
+    super();
+  }
+}
