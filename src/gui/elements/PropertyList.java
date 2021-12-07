60bdb9ce90e87c800acea579749758045a89fb44
e-seng <eth.sengsavang@gmail.com>
2021-12-07 14:17:46 -0700

    Add empty property list element to show multiple properties

---
+++ src/gui/elements/PropertyList.java
@@ -0,0 +1,19 @@
+/**
+ * @author Ethan Sengsavang
+ * <a href="mailto:ethan.sengsavang@ucalgary.ca">email</a>
+ *
+ * This will list core deatils of properties within a list.
+ * This list will be composed of multiple boxes with different properties.
+ *
+ * Each of these properties will be contained within a clickable button, which
+ * will update the display to show more details about the property.
+ */
+package gui.elements;
+
+import java.swing.JPanel;
+
+public class PropertyList extends JPanel{
+  public PropertyList(){
+    super();
+  }
+}
