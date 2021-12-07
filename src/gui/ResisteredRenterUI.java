ccd79f4c1b82dc4e16b0a6bbfefd73c39f7a41af
e-seng <eth.sengsavang@gmail.com>
2021-12-07 14:02:54 -0700

    Add Registred Renter UI as a copy of Unregistered Renter UI

---
+++ src/gui/ResisteredRenterUI.java
@@ -0,0 +1,41 @@
+/**
+ * @author Ethan Sengsavang
+ * <a href="mailto:ethan.sengsavang@ucalgary.ca">email</a>
+ *
+ * The user interface an unregistered renter will view when they enter the
+ * application.
+ *
+ * Unregistered renters should have the ability to search for and view
+ * listed properties.
+ */
+package gui;
+
+import gui.elements.SearchMenu;
+
+import java.awt.Component;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+
+public class RegisteredRenterUI extends FocusPanel{
+  // private RegisteredRenter userRepresntation;
+
+  public UnregisteredRenterUI(){
+    super();
+    init();
+  }
+
+  /**
+   * Initialize the current user interface for unregistered renters.
+   */
+  public void init(){
+    panelLayout = new GridBagLayout();
+    setLayout(panelLayout);
+
+    // Create necessary elements
+    SearchMenu search = new SearchMenu();
+
+    // populate window assuming a 4-column, 5-row grid.
+    addWidget(search, 0, 0, 1, 5);
+  }
+}
