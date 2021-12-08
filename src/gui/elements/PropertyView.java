3bafbffd6e0e1188e13d982bbbc1d19ffb986e6b
Ethan Sengsavang <eth.sengsavang@gmail.com>
2021-12-08 09:23:42 -0700

    Add PropertyView class

---
+++ src/gui/elements/PropertyView.java
@@ -0,0 +1,20 @@
+/**
+ * @author Ethan Sengsavang
+ * <a href="mailto:ethan.sengsavang@ucalgary.ca">email</a>
+ *
+ * Each instance of this element will represent an instance of Property.
+ * When viewing a list of properties, the list should create multiple instances
+ * of this object for users to see properties they wish to see.
+ *
+ * This element briefly summarizes information on the property, including:
+ * - city quadrant
+ * - housing type
+ * - furnish state
+ */
+package gui.elements;
+
+public class PropertyView extends JPanel{
+  public PropertyView(){
+    super();
+  }
+}
