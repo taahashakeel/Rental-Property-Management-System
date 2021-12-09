7b780a1566441e9f60c3eb34ff3a4149580a1783
Ethan Sengsavang <eth.sengsavang@gmail.com>
2021-12-09 16:11:29 -0700

    Move package definition to the top of the file

--- src/BackEnd/DatabaseController.java
+++ src/BackEnd/DatabaseController.java
@@ -1,11 +1,11 @@
+package BackEnd;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.*;
-
-package BackEnd;
 
 public class DatabaseController {
 
