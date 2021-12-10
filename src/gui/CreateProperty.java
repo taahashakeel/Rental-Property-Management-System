49ede3fc9d7936a9eaf273023182a21a316f3e4e
Ethan Sengsavang <eth.sengsavang@gmail.com>
2021-12-10 14:32:23 -0700

    Import necessary elements to create a property

--- src/gui/CreateProperty.java
+++ src/gui/CreateProperty.java
@@ -20,6 +20,13 @@ package gui;
 import Employee.Landlord;
 import Employee.Property;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
 public class CreateProperty extends FocusPanel{
   private Landlord propertyOwner;
 
