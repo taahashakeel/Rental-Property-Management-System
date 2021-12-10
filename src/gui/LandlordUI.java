3333b05b39003f5b3a56648ddb5d977228ade634
Ethan Sengsavang <eth.sengsavang@gmail.com>
2021-12-09 17:43:51 -0700

    Create a PropertyList for Landlords

--- src/gui/LandlordUI.java
+++ src/gui/LandlordUI.java
@@ -10,7 +10,11 @@
  */
 package gui;
 
+import gui.elements.PropertyList;
+import gui.elements.PropertyEdit;
+
 import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
 
 public class LandlordUI extends FocusPanel{
   // private Landlord userRepresentation;
@@ -27,7 +31,32 @@ public class LandlordUI extends FocusPanel{
     setLayout(panelLayout);
 
     // Create elements
+    // create dummy properties for now.
+    PropertyEdit[] dummyList = new PropertyEdit[10];
+
+    String[] statuses = {
+      "Active",
+      "Rented",
+      "Cancelled",
+      "Suspended"
+    };
+
+    for(int i = 0; i < 10; i++){
+      dummyList[i] = new PropertyEdit("415" + i + " Eagle Drive",
+          "NE",
+          "Condo",
+          i % 2 == 0,
+          415,
+          statuses[i % 4]);
+    }
+
+    PropertyList<PropertyEdit> results = new PropertyList<PropertyEdit>(dummyList);
 
     // populate window
+    GridBagConstraints gbc = FocusPanel.generateConstraints(0, 0, 1, 1);
+    gbc.fill = GridBagConstraints.BOTH;
+    gbc.weightx = 100;
+    gbc.weighty = 100;
+    add(results, gbc);
   }
 }
