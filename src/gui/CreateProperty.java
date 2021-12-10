ba82ec130c94dcbfc779e1b491332f89aeca8c8f
Ethan Sengsavang <eth.sengsavang@gmail.com>
2021-12-10 14:27:36 -0700

    Define CreateProperty class panel
    this panel will allow landlords to create new properties for them to
    list.

---
+++ src/gui/CreateProperty.java
@@ -0,0 +1,29 @@
+/**
+ * @author Ethan Sengsavang
+ * <a href="mailto:ethan.sengsavang@ucalgary.ca">email</a>
+ *
+ * the user interface that the landlord can use to generate new Properties.
+ *
+ * Within this section, a new property instance will be created by the Landlord
+ * that will be associated to whichever landlord created it.
+ *
+ * Landlords have the ability to specify the following details of a property:
+ * - Address
+ * - Quadrant
+ * - Number of bathrooms
+ * - Number of bedrooms
+ * - if the property is furnished
+ * - Monthy rental cost
+ */
+package gui;
+
+import Employee.Landlord;
+import Employee.Property;
+
+public class CreateProperty extends FocusPanel{
+  private Landlord propertyOwner;
+
+  public CreateProperty(Landlord propertyOwner){
+    this.propertyOwner = propertyOwner;
+  }
+}
